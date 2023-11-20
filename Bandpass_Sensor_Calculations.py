b5bbd7a69ad577fb3e6d16a543d41dbae675c996
Duncan Lyster <duncan.lyster@physics.ox.ac.uk>
2023-11-20 18:30:49 +0000

    Integrated Bandpass_Sen... with TIDT. Haven't yet checked thoroughly for bugs. Currently passing hard coded sensor values (not from JSON file). There is a bug in Bandpass... that means it can't take wavelength ranges outside the defaults.

--- Bandpass_Sensor_Calculations.py
+++ Bandpass_Sensor_Calculations.py
@@ -3,12 +3,12 @@
 
 Original: Created on Thu Aug 28 22:02:53 2014 Tweaked for LTM/TIRI/Icy Moons 3/5/22 @author: bowles """
 
-from numpy import exp, sqrt, loadtxt, tan
-from matplotlib import pyplot as plt
-from scipy import integrate, constants
-from scipy.constants import h, c, Boltzmann
-from tqdm import tqdm
 import numpy as np
+
+from numpy import exp, sqrt, loadtxt, tan
+from scipy import integrate, constants
+from scipy.constants import h, c, k
+from tqdm import tqdm
 
 def calculate_spectral_radiance(temp, wavelength):
     """
@@ -23,7 +23,7 @@ def calculate_spectral_radiance(temp, wavelength):
     """
     # Radiation constants
     C1 = 2 * h * (c ** 2)
-    C2 = (h * c) / Boltzmann
+    C2 = (h * c) / k
 
     # Calculate radiance using Planck's law
     exponent = C2 / (wavelength * temp)
@@ -40,7 +40,7 @@ def calculate_spectral_radiance_derivative(temp, wavelength):
 
     # Radiation constants
     C3 = constants.pi * constants.h * (constants.c ** 2)
-    C4 = (constants.h * constants.c) / constants.Boltzmann
+    C4 = (constants.h * constants.c) / constants.k
 
     radiance1 = C3 * C4 * exp(C4 / (temp * wavelength))
     radiance2 = ((temp ** 2) * (wavelength ** 6)) * ((exp(C4 / (temp * wavelength)) - 1) ** 2)
@@ -67,17 +67,17 @@ def deltaf_calc (orbital_altitude, target_mass, target_radius, Half_angle):
 
 class SNR_data_per_band:
     
-    wavenumber_array =[]
-    temperature_snr_array =[]
-    emissivity_snr_array =[]
-    scene_temperature = []
-    bandpass = []
-    nedt_array =[]
-    ner_array=[]
-    dBdT_array = [] #This is the derivative of the radiance with respect to temperature
-    power_detector_array=[]
+    wavenumber_array = []
+    scene_temperatures_array = []
+    bandpass_array = []
+    temperature_snr = 0.0
+    emissivity_snr = 0.0
+    nedt = 0.0
+    ner = 0.0
+    dBdT = 0.0
+    power_detector = 0.0
     
-def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diameter, detector_absorption, det_fnumber, orbital_altitude, target_mass, target_radius):
+def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diameter, detector_absorption, det_fnumber, orbital_altitude, target_mass, target_radius, temperature_array, Dstar, TDI_pixels):
     """
     This function calculates the SNR for a given set of parameters. BUG: Cannot currently deal with correct wavenumber input values for ETM. Numbers in main function are chosen to ensure this runs but they are not right. 
     """
@@ -95,16 +95,11 @@ def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diame
     #DERIVED ORBITAL PARAMETERS
     deltaf = deltaf_calc (orbital_altitude, target_mass, target_radius, Half_angle)
 
-    #Icy moon Temperatures
-    temperature_array = np.arange(45.0,180.0,10.0) #This will have to go somehow when it takes in spectral array from the thermal instrument model. 
-    
     #Setup the arrays to hold the results
-    snr_array = np.zeros(temperature_array.size)
-    snr_delta_power = np.zeros(temperature_array.size)
-    NER_array = np.zeros(temperature_array.size)
-    NEDT_array=np.zeros(temperature_array.size)
-    integrated_deriv_radiance_array = np.zeros(temperature_array.size)
-    power_at_detector_array = np.zeros(temperature_array.size)
+    snr = 0.0
+    snr_delta_power = 0.0
+    integrated_deriv_radiance = 0.0
+    integrated_power_at_detector = 0.0
    
     #Load the bandpass file
     bandpass_file = "nightingale_bandpass.txt"              #This is the bandpass file for the Nightingale mission
@@ -112,8 +107,8 @@ def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diame
     number_of_bands = len(raw_band_in)                      #determine the number of bands in the file
     
     #Calculate the NEP for the detector
-    NEP = (sqrt(detector_area)*100.0)*sqrt(deltaf)/Dstar
-    NEP = NEP/sqrt(TDI_pixels) #This is the NEP per pixel, so we need to scale it for the number of pixels in the TDI.
+    noise_eq_power = (sqrt(detector_area)*100.0)*sqrt(deltaf)/Dstar
+    noise_eq_power = noise_eq_power/sqrt(TDI_pixels) #This is the NEP per pixel, so we need to scale it for the number of pixels in the TDI.
 
     bandpass_results_array = [] #create an array to hold the results for each bandpass
     total_throughput_array = np.full(wavelength_array.size, total_throughput) #create an array of the total throughput for each wavelength
@@ -125,26 +120,31 @@ def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diame
         lower_index=np.where(wavelength_array>bandpass_array[0]*1E-6)[0][0] #convert the bandpass wavelengths from microns to m and find the index of the first wavelength in the bandpass
         upper_index=np.where(wavelength_array>bandpass_array[1]*1E-6)[0][0] #convert the bandpass wavelengths from microns to m and find the index of the last wavelength in the bandpass
             
-        filter_wavelength_array = wavelength_array[lower_index:upper_index] #create an array of the wavelengths in the bandpass
-        filter_throughput_array = total_throughput_array[lower_index:upper_index] #create an array of the total throughput for each wavelength in the bandpass
+        filter_wavelength_array = wavelength_array[lower_index:upper_index]         #create an array of the wavelengths in the bandpass
+        filter_throughput_array = total_throughput_array[lower_index:upper_index]   #create an array of the total throughput for each wavelength in the bandpass
         
         #Setup the arrays to hold the results
         bandpass_results = SNR_data_per_band () #create a new results object for each bandpass
         radiance_array = np.zeros(filter_wavelength_array.size)
         deriv_radiance_array = np.zeros(filter_wavelength_array.size)
-        bandpass_results.temperature_snr_array=np.zeros(temperature_array.size)
-        bandpass_results.emissivity_snr_array=np.zeros(temperature_array.size)
-        bandpass_results.nedt_array=np.zeros(temperature_array.size)
-        bandpass_results.ner_array=np.zeros(temperature_array.size)
-        bandpass_results.dBdT_array=np.zeros(temperature_array.size)
-        bandpass_results.power_detector_array=np.zeros(temperature_array.size)
+        bandpass_results.temperature_snr = 0.0
+        bandpass_results.emissivity_snr = 0.0
+        bandpass_results.nedt = 0.0
+        bandpass_results.ner = 0.0
+        bandpass_results.dBdT = 0.0
+        bandpass_results.power_detector = 0.0
         
-        integrated_NEP =(sqrt(detector_area)*100)*sqrt(deltaf)/(Dstar*np.mean(filter_throughput_array))
-        integrated_NEP = integrated_NEP/sqrt(TDI_pixels)
+        integrated_NEP = (sqrt(detector_area)*100)*sqrt(deltaf)/(Dstar*np.mean(filter_throughput_array))
+        integrated_NEP = integrated_NEP / sqrt(TDI_pixels)
+        integrated_NER = integrated_NEP / AOmega 
+
+        # Initialize accumulators
+        total_power_at_detector = 0.0
+        total_deriv_radiance = 0.0
         
-        # Loop over the temperatures and calculate the SNR for each temperature
+        # Loop over the temperatures and calculate the SNR for each temperature in the temperature array
         for temp_index in range(temperature_array.size):
-            # Calculate radiance and its derivative for each wavelength in the bandpass
+            # Calculate radiance and its derivative for each wavelength in the bandpass for the current temperature
             for wavelength_index in range(filter_wavelength_array.size):
                 temperature = temperature_array[temp_index]
                 wavelength = filter_wavelength_array[wavelength_index]
@@ -155,39 +155,37 @@ def calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diame
             flux_at_detector = radiance_array * filter_throughput_array * det_solid_angle
             power_at_detector = flux_at_detector * detector_area
 
-            # Integrate the power and the derivative of the radiance over the bandpass
-            integrated_power_at_detector = integrate.simps(power_at_detector, filter_wavelength_array)
-            integrated_deriv_radiance = integrate.simps(deriv_radiance_array, filter_wavelength_array)
+            # Integrate the power and the derivative of the radiance over the bandpass scaling by number of temperatures in fov
+            integrated_power_at_detector = integrate.simps(power_at_detector, filter_wavelength_array) / temperature_array.size
+            integrated_deriv_radiance = integrate.simps(deriv_radiance_array, filter_wavelength_array) / temperature_array.size
 
-            # Store the derivative of the radiance and the power at the detector for each temperature
-            integrated_deriv_radiance_array[temp_index] = integrated_deriv_radiance
-            power_at_detector_array[temp_index] = integrated_power_at_detector
+            total_power_at_detector += integrated_power_at_detector
+            total_deriv_radiance += integrated_deriv_radiance
 
-            # Calculate the SNR for the detector
-            snr_array[temp_index] = integrated_power_at_detector / NEP
-            snr_delta_power[temp_index] = (integrated_power_at_detector - (0.99 * integrated_power_at_detector)) / NEP #This is the SNR for a 1% change in emissivity
+        # Calculate the SNR for the detector
+        snr = total_power_at_detector / noise_eq_power
+        snr_delta_power = (total_power_at_detector - (0.99 * total_power_at_detector)) / noise_eq_power #This is the SNR for a 1% change in emissivity
 
-            # Calculate the NEDT for the detector
-            integrated_NER = integrated_NEP / AOmega 
-            NEDT_array[temp_index] = integrated_NER / integrated_deriv_radiance
-            NER_array[temp_index] = integrated_NER
+        # Calculate the NEDT for the detector
+        noise_eq_delta_T = integrated_NER / total_deriv_radiance
 
-            # Store results in bandpass_results
-            bandpass_results.emissivity_snr_array[temp_index] = snr_delta_power[temp_index]
-            bandpass_results.temperature_snr_array[temp_index] = snr_array[temp_index]
-            bandpass_results.nedt_array[temp_index] = NEDT_array[temp_index]
-            bandpass_results.ner_array[temp_index] = NER_array[temp_index]
-            bandpass_results.dBdT_array[temp_index] = integrated_deriv_radiance_array[temp_index]
-            bandpass_results.power_detector_array[temp_index] = power_at_detector_array[temp_index]
-        
+        # Store results in bandpass_results
+        bandpass_results.emissivity_snr = snr_delta_power
+        bandpass_results.temperature_snr = snr
+        bandpass_results.nedt = noise_eq_delta_T
+        bandpass_results.ner = integrated_NER
+        bandpass_results.dBdT = total_deriv_radiance
+        bandpass_results.power_detector = total_power_at_detector
+    
         #Store the results for each bandpass in an array
-        bandpass_results.scene_temperature = temperature_array 
+        bandpass_results.scene_temperatures_array = temperature_array 
         bandpass_results.wavenumber_array=filter_wavelength_array
-        bandpass_results.bandpass = bandpass_array
+        bandpass_results.bandpass_array = bandpass_array
         bandpass_results_array.append(bandpass_results)
 
     return bandpass_results_array
 
+'''
 if __name__ == "__main__":
 
     #WAVENUMBER RANGE chosen to give lambda range of 1-300 microns
@@ -207,6 +205,23 @@ if __name__ == "__main__":
     target_mass = 7.34767309E22     #Mass of the target body in kg
     target_radius = 1737E3          #radius of target body in m
 
-    bandpass_results_array = calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diameter, detector_absorption, det_fnumber, orbital_altitude, target_mass, target_radius)
+    #Load the temperature array from the test tile
+    temperatures = np.loadtxt("test_tile.csv", delimiter=',', encoding='utf-8-sig')
+
+    # Flatten the 2D temperatures array into a 1D array
+    temperature_array = temperatures.flatten()
+
+    bandpass_results_array = calculate_snr(wavenumber_min, wavenumber_max, detector_area, telescope_diameter, detector_absorption, det_fnumber, orbital_altitude, target_mass, target_radius, temperature_array, Dstar, TDI_pixels)
     
+    #Print the results
+    for bandpass_results in bandpass_results_array:
+        print("Bandpass: ", bandpass_results.bandpass_array)
+        print("Temperature SNR: ", bandpass_results.temperature_snr)
+        print("Emissivity SNR: ", bandpass_results.emissivity_snr)
+        print("NEDT: ", bandpass_results.nedt)
+        print("NER: ", bandpass_results.ner)
+        print("dB/dT: ", bandpass_results.dBdT)
+        print("Power at detector: ", bandpass_results.power_detector)
+
     print("Done!")
+'''
